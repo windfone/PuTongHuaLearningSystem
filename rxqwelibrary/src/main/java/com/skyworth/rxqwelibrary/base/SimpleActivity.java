fd97a0e2cc01ffec2075dfec337d77948431613d
ZHANGGUIHUA <9187712813>
2019-08-19 15:03:15 +0800

    android-华为5.1.1 沉浸式无效的解决方法探索-暂未使用

--- rxqwelibrary/src/main/java/com/skyworth/rxqwelibrary/base/SimpleActivity.java
+++ rxqwelibrary/src/main/java/com/skyworth/rxqwelibrary/base/SimpleActivity.java
@@ -1,6 +1,7 @@
 package com.skyworth.rxqwelibrary.base;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 
 import com.skyworth.rxqwelibrary.managers.AppManagers;
 import com.umeng.analytics.MobclickAgent;
+
+import java.lang.reflect.Method;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -58,7 +61,39 @@ public abstract class SimpleActivity extends AppCompatActivity {
         }
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+//        transportStatus(this);
     }
+
+    // 华为平板沉浸式- 设置透明状态栏无效 解决方法
+    /*public static void transportStatus(Activity context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            context.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            if (!isHaveNavigationBar(context))
+                context.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+    public static boolean isHaveNavigationBar(Activity context) {
+
+        boolean isHave = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            isHave = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                isHave = false;
+            } else if ("0".equals(navBarOverride)) {
+                isHave = true;
+            }
+        } catch (Exception e) {
+            Log.w("TAG", e);
+        }
+        return isHave;
+    }*/
 
     protected void onViewCreated() {
 
